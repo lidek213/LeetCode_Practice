a7a88774be0158b521a8859018d4877c3cc3177b
lidek213 <lidek213@gmail.com>
2023-07-18 15:36:34 +0800

    Add Problem2396.cpp & Problem2396.py

---
+++ cpp/Problem2396.cpp
@@ -0,0 +1,13 @@
+/*
+ * Problem: 2396. Strictly Palindromic Number
+ * URL: https://leetcode.com/problems/strictly-palindromic-number/
+ * Difficulty: Medium
+ * Tags: Math, Intuition
+ */
+
+class Solution {
+public:
+	bool isStrictlyPalindromic(int n) {
+		return false;
+	}
+};
---
+++ python/Problem2396.py
@@ -0,0 +1,8 @@
+# Problem: 2396. Strictly Palindromic Number
+# URL: https://leetcode.com/problems/strictly-palindromic-number/
+# Difficulty: Medium
+# Tags: Math, Intuition
+
+class Solution:
+  def isStrictlyPalindromic(self, n: int) -> bool:
+    return False
